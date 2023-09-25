fbb9bc0e0c4f956530a90d8913d43e7bebc44482
Antonela Borgogno <antonelaborgogno@gmail.com>
2023-09-25 18:09:17 -0300

    contenido y css de usuario

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Usuario from './componentes/Usuario';
 function App() { //componente funcional, el nombre del componente va en mayuscula
   return (
     <div className="App"> 
-      <Usuario imagen={require('./assets/imagenes/perfil.webp')} nombre={'Maria'} cargo={'Gerente Gerenal'} color={'#94FCD9'}/>
-      <Usuario imagen={require('./assets/imagenes/perfil.webp')} nombre={'Dolores'} cargo={'Vice-Gerente Gerenal'} color={'#94FCD9'}/>
-      <Usuario imagen={require('./assets/imagenes/perfil.webp')} nombre={'Maria'} cargo={'Gerente Gerenal'} color={'#94FCD9'}/> 
-      <Usuario imagen={require('./assets/imagenes/perfil.webp')} nombre={'Maria'} cargo={'Gerente Gerenal'} color={'#94FCD9'}/>      
-      <Usuario imagen={require('./assets/imagenes/perfil.webp')} nombre={'Maria'} cargo={'Gerente Gerenal'} color={'#94FCD9'}/>
-
+      <Usuario imagen={require('./assets/imagenes/perfil.webp')} nombre={'Maria'} cargo={'Gerente Gerenal'} color={'#94FCD9'} conoceme={'Curriculum'}/>
+      <Usuario imagen={require('./assets/imagenes/perfil.webp')} nombre={'Dolores'} cargo={'Vice-Gerente Gerenal'} color={'#94FCD9'} conoceme={'Curriculum'}/>
+      <Usuario imagen={require('./assets/imagenes/perfil.webp')} nombre={'Edit'} cargo={'Tesorera'} color={'#94FCD9'} conoceme={'Curriculum'}/> 
+      <Usuario imagen={require('./assets/imagenes/perfil.webp')} nombre={'Matilde'} cargo={'Revisadora de Cuentas'} color={'#94FCD9'} conoceme={'Curriculum'}/>      
+      <Usuario imagen={require('./assets/imagenes/perfil.webp')} nombre={'Cintia'} cargo={'Secretaria'} color={'#94FCD9'} conoceme={'Curriculum'}/>
     </div>
   );
 }
@@ -19,5 +18,4 @@ export default App;
 //------notas------
 /*
 En Jsx las class para aplicar css se usa className
-
 */ 
--- src/componentes/Usuario.jsx
+++ src/componentes/Usuario.jsx
@@ -1,12 +1,12 @@
 function Usuario(props) {
   return (
     <div>
-        <div className="card" style={{width: '18rem', backgroundColor: props.color}}>
+        <div className="card m-4" style={{width: '18rem', backgroundColor: props.color}}>
             <img src={props.imagen} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{props.nombre}</h5>
-                <p className="card-text">{props.cargo}</p>
-                {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+                <p className="card-text">{props.cargo}<small className="text-body-secondary"></small></p>
+                <a href="##" className="btn btn-color" style={{border:'1px solid black'}}>{props.conoceme}</a>
             </div>
         </div>
     </div>
